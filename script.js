859853fee8c41dc5b702230e77c7e7782d401b55
Trenton Beckendorff <trentonbeckendorff@utexas.edu>
2017-10-29 09:53:13 -0500

    we got der dater

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-$("#volunteer-button").click(function() {
-    $('html,body').animate({
-            scrollTop: $(".card-container").offset().top},
-        'slow');
-});
+function onArrival(event) {
+    event = event || window.event; // IE
+    var target = event.target || event.srcElement; // IE
+    var button = target
+    if ($(target).is("a")) {
+        button = target.parentNode
+    }
+    button.classList.toggle('picked-up')
+    if (!button.classList.contains('picked-up')) {
+        button.innerText = 'Dropped-Off'
+        button.classList.add('dropped-off')
+    } else {
+        var card = button.parentNode.parentNode
+        $(card).remove()
+    }
+}
+
+function onVolunteerSignIn(event) {
+    event = event || window.event; // IE
+    var target = event.target || event.srcElement; // IE
+    var loginCard = document.getElementById('volunteerLoginCard')
+    console.log(loginCard.style.visibility)
+    if ($(loginCard).css('visibility') === 'hidden') {
+        loginCard.style.visibility = 'visible'
+    }
+}
